210000c639e967625ea0b2680ddeb478f7d23339
Oleg Titov <oleggtitoff@gmail.com>
2019-12-04 22:35:23 +0200

    In Salad class added methods for adding ingredients and calories calculation (all tests are green).

--- src/main/java/ua/training/model/Salad.java
+++ src/main/java/ua/training/model/Salad.java
@@ -1,4 +1,35 @@
 package ua.training.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Salad {
+    private List<Ingredient> ingredients = new ArrayList<>();
+    private double totalKCalories;
+
+    public void addVegetable(String name, double mass, double kCaloriesIn1KG) {
+        Ingredient vegetable = new Vegetable(name, mass, kCaloriesIn1KG);
+        addIngredient(vegetable);
+    }
+
+    public void addAdditionalIngredient(String name, double mass,
+                                        double kCaloriesIn1KG, String type) {
+        Ingredient additionalIngredient = new AdditionalIngredient(name, mass,
+                kCaloriesIn1KG, type);
+        addIngredient(additionalIngredient);
+    }
+
+    private void addIngredient(Ingredient ingredient) {
+        addKCaloriesToTotal(ingredient);
+        ingredients.add(ingredient);
+    }
+
+    private void addKCaloriesToTotal(Ingredient ingredient) {
+        totalKCalories += ingredient.getKCalories();
+    }
+
+    public double getTotalKCalories() {
+        return totalKCalories;
+    }
+
 }
--- src/test/java/ua/training/model/SaladTest.java
+++ src/test/java/ua/training/model/SaladTest.java
@@ -1,8 +1,25 @@
 package ua.training.model;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SaladTest {
+    private Salad salad;
+    private double doublesComparisonDelta = 0.0000000000001;
+
+    @Before
+    public void createSalad() {
+        salad = new Salad();
+    }
+
+    @Test
+    public void testTotalKCaloriesCalculation() {
+        salad.addVegetable("Potato", 0.5, 400);
+        salad.addAdditionalIngredient("Sauce", 0.1, 500, "SAUCE");
+        double totalKCalories = salad.getTotalKCalories();
+
+        Assert.assertEquals(250, totalKCalories, doublesComparisonDelta);
+    }
 
 }
