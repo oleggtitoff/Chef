a09a6f4840da25b9f164b3d2d62be160daf8ed88
Oleg Titov <oleggtitoff@gmail.com>
2019-12-04 16:56:41 +0200

    In Ingredient class added function for calculation calories in some mass  (test is green).

--- src/main/java/ua/training/model/Ingredient.java
+++ src/main/java/ua/training/model/Ingredient.java
@@ -1,4 +1,9 @@
 package ua.training.model;
 
 class Ingredient {
+
+    double calculateCaloriesInMass(double mass, double kCaloriesIn1KG) {
+        return mass * kCaloriesIn1KG;
+    }
+
 }
--- src/test/java/ua/training/model/IngredientTest.java
+++ src/test/java/ua/training/model/IngredientTest.java
@@ -1,5 +1,23 @@
 package ua.training.model;
 
-public class IngredientTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class IngredientTest {
+    private Ingredient ingredient;
+    private double doublesComparisonDelta = 0.0000000000001;
+
+    @Before
+    public void createIngredient() {
+        ingredient = new Ingredient();
+    }
+
+    @Test
+    public void testCalculateCaloriesInMass() {
+        double caloriesInThisMass
+                = ingredient.calculateCaloriesInMass(0.1, 1000);
+
+        Assert.assertEquals(100.0, caloriesInThisMass, doublesComparisonDelta);
+    }
 }
