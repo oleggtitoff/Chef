3f35fbffac25b76e98f06725cc2cebd208b86d8c
Oleg Titov <oleggtitoff@gmail.com>
2019-12-04 18:11:14 +0200

    AdditionalIngredient class implemented.

--- src/main/java/ua/training/model/AdditionalIngredient.java
+++ src/main/java/ua/training/model/AdditionalIngredient.java
@@ -1,4 +1,15 @@
 package ua.training.model;
 
-public class AdditionalIngredient {
+public class AdditionalIngredient extends Ingredient {
+    private String type;
+
+    AdditionalIngredient(double mass, double kCaloriesIn1KG, String type) {
+        super(mass, kCaloriesIn1KG);
+        this.type = type;
+    }
+
+    String getType() {
+        return type;
+    }
+
 }
