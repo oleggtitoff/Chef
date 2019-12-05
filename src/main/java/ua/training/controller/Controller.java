2fa162fa52f267dfc8739e343042b85291a867a5
Oleg Titov <oleggtitoff@gmail.com>
2019-12-05 03:47:47 +0200

    In Controller added constructor (all tests are green).

--- src/main/java/ua/training/controller/Controller.java
+++ src/main/java/ua/training/controller/Controller.java
@@ -1,4 +1,19 @@
 package ua.training.controller;
 
+import ua.training.model.Salad;
+import ua.training.view.View;
+
 public class Controller {
+    private Salad salad;
+    private View view;
+
+    Controller(Salad salad, View view) {
+        this.salad = salad;
+        this.view = view;
+    }
+
+    public void processUser() {
+        // TODO
+    }
+
 }
