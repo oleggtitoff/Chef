f550444abfe23542fe25cc1e426eb16ba5626c03
Oleg Titov <oleggtitoff@gmail.com>
2019-12-05 02:00:29 +0200

    Added StringsContainer (all tests are green).

---
+++ src/main/java/ua/training/view/StringsContainer.java
@@ -0,0 +1,22 @@
+package ua.training.view;
+
+public interface StringsContainer {
+    String SPACE_SIGN = " ";
+
+    String WRONG_INPUT_MESSAGE = "input.wrong.message";
+    String INPUT_INGREDIENT_QUESTION = "input.ingredient.question";
+    String WHAT_INGREDIENT_QUESTION = "input.what.ingredient.question";
+    String NAME_PROMPT = "input.name.prompt";
+    String MASS_PROMPT = "input.mass.prompt";
+    String K_CALORIES_PROMPT = "input.kcalories.prompt";
+    String TYPE_PROMPT = "input.type.prompt";
+    String SORT_QUESTION = "input.sort.question";
+    String RANGE_QUESTION = "input.range.question";
+    String RANGE_MIN_PROMPT = "input.range.min.prompt";
+    String RANGE_MAX_PROMPT = "input.range.max.prompt";
+    String INGREDIENT_ADDED_MESSAGE = "input.ingredient.added.message";
+    String SORT_MESSAGE = "output.sort.message";
+    String RANGE_MESSAGE = "output.range.message";
+    String TOTAL_CALORIES_MESSAGE = "output.total.calories.message";
+    String INGREDIENTS_MESSAGE = "output.ingredients.message";
+}
