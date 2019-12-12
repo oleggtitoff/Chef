74e4bba8399c2f0c1ced2ad4912593ebbc12feb4
Oleg Titov <oleggtitoff@gmail.com>
2019-12-12 13:24:40 +0200

    Added AdditionalIngredient test for calculating calories in mass (all tests are green).

--- src/test/java/ua/training/model/AdditionalIngredientTest.java
+++ src/test/java/ua/training/model/AdditionalIngredientTest.java
@@ -1,5 +1,24 @@
 package ua.training.model;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class AdditionalIngredientTest {
+    private AdditionalIngredient additionalIngredient;
+
+    @Before
+    public void createAdditionalIngredient() {
+        additionalIngredient = new AdditionalIngredient("Mayonnaise", 0.080, 6000, "SAUCES");
+    }
+
+    @Test
+    public void testCalculateCaloriesInMass() {
+        double caloriesInThisMass
+                = additionalIngredient.calculateCaloriesInMass(0.1, 1000);
+
+        Assert.assertEquals(100.0, caloriesInThisMass,
+                TestConstants.DOUBLES_COMPARISON_DELTA);
+    }
 
 }
